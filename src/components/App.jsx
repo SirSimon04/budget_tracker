f2119c4c8de2c0f2b31bd9d55fc6c27cb67b3e07
SirSimon04 <simi@engelnetz.de>
2022-02-01 22:41:42 +0100

    setup, add menu bar

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Typography } from "@mui/material";
+import Menu from "./Menu";
+// import {currencyFormatter} from "../utils";
+
+function App() {
+  return (
+    <div>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
